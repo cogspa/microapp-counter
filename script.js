e32a847354ca881bcda8c85a89df68aa2e5f1b16
cogspa <cogspa@yahoo.com>
2025-08-17 05:30:02 -0700

    feat add counter app code

---
+++ script.js
@@ -0,0 +1,7 @@
+﻿const countEl = document.getElementById("count");
+let n = 0;
+const set = v => { n = v; countEl.textContent = n; };
+
+document.getElementById("inc").onclick = () => set(n + 1);
+document.getElementById("dec").onclick = () => set(n - 1);
+document.getElementById("reset").onclick = () => set(0);
